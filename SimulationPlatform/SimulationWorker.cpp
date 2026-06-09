a64a3db5dc5d6c772cf6b247ad5aedf11a16af58
Muhammed Serhat Mutlu <muhammedserhatmutlu@gmail.com>
2026-06-09 13:04:12 +0300

    Configured CMake for Qt6 and added SimulationWorker class

---
+++ SimulationPlatform/SimulationWorker.cpp
@@ -0,0 +1,1 @@
+#include "SimulationWorker.h"
---
+++ SimulationPlatform/SimulationWorker.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include "Rigidbody.h"
+#include <QObject>
+#include <QTimer>
+#include <memory>
+
+class SimulationWorker : public QObject{
+	Q_OBjECT
+public:
+	SimulationWorker(double initVel, double angle, QObject* parent = nullptr);
+	~SimulationWorker();
+
+signals:
+	void telemetryUpdated(double x, double z, double velocity, double time);
+	void simulationFinished();
+
+public slots:
+	void startSimulation();
+	void nextStep();
+
+private:
+	std::unique_ptr<RigidBody> m_rocket;
+	QTimer* m_simTimer;
+	double m_dt;
+	double m_elapsedTime;
+}
